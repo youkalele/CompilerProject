98517fdedbecfdb9e9474ac946831bcbc13ee68c
youkalele <64647884+youkalele@users.noreply.github.com>
2025-03-22 16:04:13 -0400

    match() done

--- parser/CMinusParser.java
+++ parser/CMinusParser.java
@@ -1,9 +1,14 @@
+
+import parser.CompoundStatement;
+import scanner.Token;
+
 package parser;
 
 import javax.lang.model.util.ElementScanner14;
 
 import scanner.CMinusScanner2;
 import scanner.*;
+
 import scanner.Token.TokenType;
 
 public class CMinusParser implements Parser { //match() is meant to assert that the given token is the same as the current token AND advance the Token pointer
@@ -24,8 +29,7 @@ public class CMinusParser implements Parser { //match() is meant to assert that 
     {
         Program retProg = new Program();
         //get first token
-        while(scan.viewNextToken().getType()!=Token.TokenType.EOF)
-        {
+        while (scan.viewNextToken().getType() != Token.TokenType.EOF) {
             retProg.addDecl(parseDecl());
         }
         //Remember that program REQURES at least ONE decl; throw an error if there are no decls
@@ -33,50 +37,40 @@ public class CMinusParser implements Parser { //match() is meant to assert that 
 
     }
 
-    public Declaration parseDecl()
-    {
+    public Declaration parseDecl(boolean local) {
         Declaration retDecl = null;
-        if(scan.viewNextToken().getType()==Token.TokenType.INT)
-        {
+        if (scan.viewNextToken().getType() == Token.TokenType.INT) {
             scan.getNextToken();
-            retDecl=parseDecl1(scan.viewNextToken()); //go to decl'
-        }
-        else if(scan.viewNextToken().getType()==Token.TokenType.VOID)
-        {
-            //match(()
+            retDecl = parseDecl1(local); //go to decl'
+        } else if (scan.viewNextToken().getType() == Token.TokenType.VOID) {
+            match(TokenType.OPEN_PAREN);
             scan.getNextToken();
-            retDecl=parseFunDecl1(Token.TokenType.VOID, scan.viewNextToken());
-        }
-        else{
+            retDecl = parseFunDecl1(Token.TokenType.VOID, scan.viewNextToken());
+        } else {
             //error
         }
 
         return retDecl;
     }
 
-    public Declaration parseDecl1(Token id) //2
+    public Declaration parseDecl1(boolean local) //2
     {
+        Token id = scan.getNextToken();
         //descend into madness
-        if(scan.viewNextToken().getType()==Token.TokenType.SEMI)
-        {
+        if (scan.viewNextToken().getType() == Token.TokenType.SEMI) {
             scan.getNextToken();
             return new VarDecl(id, -1);
-        }
-        else if(scan.viewNextToken().getType()==Token.TokenType.OPEN_BRACKET)
-        {
+        } else if (scan.viewNextToken().getType() == Token.TokenType.OPEN_BRACKET) {
             int arrayVal = -1;
             scan.getNextToken();
             arrayVal = parseNum();
-            //match(])
-            //match(;)
+            match(TokenType.CLOSE_BRACKET);
+            match(TokenType.SEMI);
             return new VarDecl(id, arrayVal);//constructor needs to handle either array or not array
-        }
-        else if(scan.viewNextToken().getType()==Token.TokenType.OPEN_PAREN)
-        {
+        } else if (local && scan.viewNextToken().getType() == Token.TokenType.OPEN_PAREN) {
             scan.getNextToken();
             return parseFunDecl1(Token.TokenType.INT, id);
-        }
-        else{
+        } else {
             //error
         }
         return null;
@@ -84,44 +78,47 @@ public class CMinusParser implements Parser { //match() is meant to assert that 
 
     public Declaration parseFunDecl1(Token.TokenType type, Token id) //3
     {
-        funDecl retDecl= new funDecl(id, type); //Capitalize this later
-        if(scan.viewNextToken().getType()!=Token.TokenType.VOID)
-        {
+        funDecl retDecl = new funDecl(id, type); //Capitalize this later
+        if (scan.viewNextToken().getType() != Token.TokenType.VOID) {
             retDecl.setVoid();
-            //match( ) )
-        }
-        else{
-            while(scan.viewNextToken().getType()!=Token.TokenType.CLOSE_PAREN)
-            {
+            match(TokenType.CLOSE_PAREN);
+        } else {
+            while (scan.viewNextToken().getType() != Token.TokenType.CLOSE_PAREN) {
                 retDecl.addParam(parseParam());
             }
         }
 
         retDecl.setBody(parseCompoundStatement());
 
-        
         return retDecl;
     }
 
     public Param parseParam() //4
     {
-        //match(int)
+        match(TokenType.INT);
         Token idToken = scan.getNextToken();
         boolean isArr = false;
-        if(scan.viewNextToken().getType()==TokenType.OPEN_BRACKET)
-        {
-            //match(])
+        if (scan.viewNextToken().getType() == TokenType.OPEN_BRACKET) {
+            match(TokenType.CLOSE_BRACKET);
             isArr = true;
         }
 
         return new Param(idToken, isArr);
-        
+
     }
 
     public CompoundStatement parseCompoundStatement() //5
     {
-        //match({)
+        match(TokenType.OPEN_CURLY);
+
+        CompoundStatement retStmt = new CompoundStatement();
         //while decls addDecl()
+        while (scan.viewNextToken().getType() == TokenType.INT) {
+            retStmt.addDecl(parseDecl(true));
+        }
+        while (scan.viewNextToken().getType() == TokenType.CLOSE_CURLY) {
+            retStmt.addStmt(parseStatement());
+        }
         //while stmt addStmt()
         //How to ensure that all decls come before all stmts? perchance generate the first/follow set? all local decls do have to start with int and no stmts do tho
         return null;
@@ -225,5 +222,13 @@ public class CMinusParser implements Parser { //match() is meant to assert that 
     public int parseNum() //25
     {
         return 0;
+    }
+
+    public void match(TokenType t) {
+        if (scan.viewNextToken().getType() == t) {
+            scan.getNextToken();
+        } else {
+            //hold an error for this
+        }
     }
 }
