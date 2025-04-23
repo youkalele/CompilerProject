30e98528cc5c9d8ec90d68660a49784e31aa64d7
youkalele <64647884+youkalele@users.noreply.github.com>
2025-04-22 22:13:01 -0400

    Started Compiler class

---
+++ CMinusCompiler.java
@@ -0,0 +1,33 @@
+import compiler.*;
+import parser.*;
+import lowlevel.*;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class CMinusCompiler implements Compiler {
+
+
+    public CMinusCompiler() {
+    }
+
+    public void compile(String filePrefix) {
+
+        String fileName = filePrefix + ".c-";
+        try {
+           CMinusParser parsingBoy = new CMinusParser(fileName);
+           Program ast = parsingBoy.parse();
+
+           CodeItem head = ast.genLLcode(); //FIXME starting at program and going down the tree: returns the head of the linked list
+
+        } catch (IOException ioe) {
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String filePrefix = args[1];
+        CMinusCompiler myCompiler = new CMinusCompiler();
+        myCompiler.compile(filePrefix);
+    }
+}
